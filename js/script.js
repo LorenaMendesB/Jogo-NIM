251074df5a40d65bf49da295e1f2f305698c6647
Your Nam <Jpsilvabarr@gmail.com>
2022-03-18 11:49:30 -0300

    1.0.0.1

--- js/script.js
+++ js/script.js
@@ -3,11 +3,24 @@ var jogadores = {
   jogador2: { pontos: 0 },
 };
 
+const NOME_ARQUIVO = 'ranque.json';
 
+var ranque = {
+  nome: "",
+  score: "",
+};
 
+ranque = [];
 
-function start() {
-
-
-
+function escreverArquivoRanque(objeto) {
+    let objJson = JSON.stringify(objeto);
+    let arquivo = Blob(objJson, {type: 'application/javascript;charset=utf-8' });
+    let link = window.URL.createObjectURL(arquivo);
+    window.location = link;
 }
+
+function lerArquivoRanque() {
+   ranque = JSON.parse();
+}
+
+function start() {}
