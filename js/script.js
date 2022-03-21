9ae461b6c31127ea204f12c65707998e6dc219eb
Your Nam <Jpsilvabarr@gmail.com>
2022-03-21 11:00:44 -0300

    1.0.0.4

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var jogadores = {
   jogador2: { pontos: 0, fosforos: 0 },
 };
 var listaIdsFosforos = [];
-var colunas
+var colunas;
 // Constante com o nome do arquivo com o ranque.
 const NOME_ARQUIVO = "ranque.json";
 
@@ -30,22 +30,32 @@ function lerArquivoRanque() {
 
 // Função para manipular as jogadas do multiplayer de dois jogadores
 function multiplayerDoisJogadores() {
+  colunas = {
+    coluna1: { restos: 1, pegos: 0 },
+    coluna2: { restos: 3, pegos: 0 },
+    coluna3: { restos: 5, pegos: 0 },
+    coluna4: { restos: 7, pegos: 0 },
+  };
   jogadores.vezDe = 1;
   jogadores.jogador1.pontos = 0;
   jogadores.jogador2.pontos = 0;
-  document.getElementById('textoPlay2').textContent = 'Player 2';
-  document.getElementById('telaOpcoesID').style.visibility = "hidden";
-
+  document.getElementById("textoPlay2").textContent = "Player 2";
+  document.getElementById("telaOpcoesID").style.visibility = "hidden";
 }
 
 // Função para manipular as jogadas do multiplayer versus computador.
 function multiplayerVSComputador() {
-  document.getElementById('textoPlay2').textContent = 'Computador';
-  document.getElementById('telaOpcoesID').style.visibility = "hidden";
-  
+  colunas = {
+    coluna1: { restos: 1, pegos: 0 },
+    coluna2: { restos: 3, pegos: 0 },
+    coluna3: { restos: 5, pegos: 0 },
+    coluna4: { restos: 7, pegos: 0 },
+  };
+  document.getElementById("textoPlay2").textContent = "Computador";
+  document.getElementById("telaOpcoesID").style.visibility = "hidden";
 }
 
-// Função para adicionar o novo recorde e salvar em um arquivo local. 
+// Função para adicionar o novo recorde e salvar em um arquivo local.
 function salvarRecord() {
   ranque.nome = document.getElementById("entradaNome").textContent;
   ranque.score = document.getElementById("recorde").textContent;
@@ -62,56 +72,81 @@ function salvarRecord() {
 }
 
 // Função para decidir as jogadas do computador.
-function computador() {
-}
+function computador() {}
 
 // Função start iniciar o jogo NIM.
 function start() {
-  colunas = {coluna1: 1, coluna2: 3, coluna3: 5, coluna4: 7};
   listaRanques = lerArquivoRanque();
-
 }
 
-function hoverFosforos(obj){
+function hoverFosforos(obj) {
   let aux = obj.path[2].children;
   document.getElementById(obj.path[0].id).style.fillOpacity = 0.5;
-  for(let i = 0; i < aux.length; i++){
-    if(aux[i].children[1].id == obj.path[0].id)
-      break;
-      document.getElementById(aux[i].children[1].id).style.fillOpacity = 0.5;  
+  for (let i = 0; i < aux.length; i++) {
+    if (aux[i].children[1].id == obj.path[0].id) break;
+    document.getElementById(aux[i].children[1].id).style.fillOpacity = 0.5;
   }
 }
-function removeStroke(obj){
+function removeStroke(obj) {
   let aux = obj.path[2].children;
   document.getElementById(obj.path[0].id).style.fillOpacity = 0;
-  for(let i = 0; i <  aux.length; i++){
-    if(aux[i].children[1].id == obj.path[1].id)
-      break;
-      document.getElementById(aux[i].children[1].id).style.fillOpacity = 0;  
+  for (let i = 0; i < aux.length; i++) {
+    if (aux[i].children[1].id == obj.path[1].id) break;
+    document.getElementById(aux[i].children[1].id).style.fillOpacity = 0;
   }
 }
 
-function pegarFosforos(obj){
-  let aux = ''+ obj.path[1].id;
+function pegarFosforos(obj) {
+  let aux = "" + obj.path[1].id, quantidade = 0;
+  let classNome = '' + document.getElementById(aux).parentElement.classList[0];
   let listaIds = document.getElementById(aux).parentElement.children;
-  for(let i = 0; i < listaIds.length; i++){
-    if(listaIds[i].id == aux){
-      document.getElementById(aux).style.visibility = 'hidden';
+  for (let i = 0; i < listaIds.length; i++) {
+    if (listaIds[i].id == aux) {
+      document.getElementById(aux).style.visibility = "hidden";
+      quantidade++;
       break;
     }
-    document.getElementById(listaIds[i].id).style.visibility = 'hidden';
+    document.getElementById(listaIds[i].id).style.visibility = "hidden";
+    quantidade++;
   }
+  quantidade = quantidade - colunas[classNome].pegos;
+  colunas[classNome].restos -= quantidade;
+  colunas[classNome].pegos  += quantidade;
+
+  if(jogadores.vezDe == 1){
+    jogadores.jogador1.fosforos += quantidade;
+    jogadores.jogador1.pontos += quantidade;
+    document.getElementById('scorePlay1').textContent = jogadores.jogador1.fosforos;
+    jogadores.vezDe = 2;
+  }else if(jogadores.vezDe == 2){
+    jogadores.jogador2.fosforos += quantidade;
+    jogadores.jogador2.pontos += quantidade;
+    document.getElementById('scorePlay2').textContent = jogadores.jogador2.fosforos;
+    jogadores.vezDe = 1;
+  }
+
+
 }
 
 // Função para deixar as telas invisíveis ao entrar na página.
-window.onload = function (){
-  document.getElementById('TelaWinGameID').style.visibility = "hidden";
-  document.getElementById("btnVersPC").addEventListener("click", multiplayerVSComputador);
-  document.getElementById("btnVersPlayers").addEventListener("click", multiplayerDoisJogadores);
-  let colunas = document.getElementsByClassName('fosforoGrupo');
-  for(let i = 0; i < colunas.length; i++){
-    document.getElementById(colunas[i].children[1].id).addEventListener("mouseover", hoverFosforos);
-    document.getElementById(colunas[i].children[1].id).addEventListener("mouseout", removeStroke);
-    document.getElementById(colunas[i].children[1].id).addEventListener("click", pegarFosforos);
+window.onload = function () {
+  document.getElementById("TelaWinGameID").style.visibility = "hidden";
+  document
+    .getElementById("btnVersPC")
+    .addEventListener("click", multiplayerVSComputador);
+  document
+    .getElementById("btnVersPlayers")
+    .addEventListener("click", multiplayerDoisJogadores);
+  let colunas = document.getElementsByClassName("fosforoGrupo");
+  for (let i = 0; i < colunas.length; i++) {
+    document
+      .getElementById(colunas[i].children[1].id)
+      .addEventListener("mouseover", hoverFosforos);
+    document
+      .getElementById(colunas[i].children[1].id)
+      .addEventListener("mouseout", removeStroke);
+    document
+      .getElementById(colunas[i].children[1].id)
+      .addEventListener("click", pegarFosforos);
   }
-}
+};
